912f79bb7701f651eecd0cda9e3f6565732c69e6
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-06 10:03:30 -0300

    Add files via upload

---
+++ PriceQuoteApplicationAssignment/PriceQuoteApplicationAssignment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceQuoteApplicationAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+        if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return;
+            }
+           
+
+            Console.WriteLine("Please enter the package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int dimensions = (width + height + length) * 50 / 100;
+          
+
+            if (width + height + length < 50 )
+            {
+                Console.WriteLine("Your estimated total for shipping this package is: "); Console.WriteLine(dimensions);
+                
+                Console.ReadLine();
+
+            }
+
+            else if (width + height + length >= 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return;
+            }
+
+
+        }
+    }
+}
