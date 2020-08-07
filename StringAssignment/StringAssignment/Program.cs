8ab675423be3e7cd821752adff975075a3bfd52a
Jacqueline McQuaid <67380308+jacquelinemcquaid@users.noreply.github.com>
2020-08-07 11:01:49 -0300

    Add files via upload

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = "Jacqueline";
+            string lastname = "McQuaid";
+
+            Console.WriteLine(firstname + lastname);
+            Console.WriteLine("Type 'okay' to continue.");
+            Console.ReadLine();
+
+            int number1 = 3;
+            int number2 = 8;
+
+            Console.WriteLine(number1 + number2);
+            Console.WriteLine("Type 'okay' to continue.");
+            Console.ReadLine();
+
+            string favWord1 = "delicious";
+            string favWord2 = " food";
+
+            Console.WriteLine(favWord1 + favWord2);
+            Console.WriteLine("Type 'okay' to continue.");
+            Console.ReadLine();
+
+            firstname = firstname.ToUpper();
+            Console.WriteLine(firstname);
+            Console.WriteLine("Type 'okay' to continue");
+            Console.ReadLine();
+
+            string quote = "To the world you may be one person; \n but to one person you may be the world. \n \n \t Be confident. \n \t Be you.";
+            Console.WriteLine(quote);
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
